6179c76f930c87e546be823c5dddf8e5f32afa84
Andrew McInally <realfunnyestate@yahoo.com>
2021-08-05 09:34:43 -0600

    adding highscores locally

--- scoreboard.js
+++ scoreboard.js
@@ -2,12 +2,26 @@ var username = document.getElementById("username");
 var saveScoreBtn = document.getElementById("saveScoreBtn");
 var mostRecentScore = localStorage.getItem("mostRecentScore");
 var finalScore = document.getElementById("finalScore");
-finalScore.innerText = mostRecentScore;
+var MAX_HIGH_SCORES;
+var highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 
+finalScore.innerText = mostRecentScore;
 username.addEventListener("keyup", () => {
   saveScoreBtn.disabled = !username.value;
 });
 
 saveHighScore = (e) => {
   e.preventDefault();
+  var score = {
+    score: mostRecentScore,
+    name: username.value,
+  };
+  highScores.push(score);
+  highScores.sort(function (a, b) {
+    return b.score - a.score;
+  });
+  highScores.splice(10);
+  localStorage.setItem("highScores", JSON.stringify(highScores));
+  
+  
 };
